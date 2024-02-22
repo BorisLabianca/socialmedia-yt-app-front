4c20d28adad390251270d47c6b73af0ab744cd21
Boris Labianca <boris.labianca@hotmail.fr>
2024-02-22 10:08:40 -0700

    completed reset password page layout

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,5 +1,65 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { CustomButton, Loading, TextInput } from "../components";
+
 const ResetPassword = () => {
-  return <div>ResetPassword</div>;
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onChange" });
+
+  const onSubmit = async (data) => {};
+
+  return (
+    <div className="w-full h-[100vh] bg-bgColor flex items-center justify-center p-6">
+      <div className="bg-primary w-full md:w-1/3 2xl:w-1/4 px-6 py-8 shadow-md rounded-lg">
+        <p className="text-ascent-1 text-lg font-semibold">Email Address</p>
+        <span className="text-sm text-ascent-2">
+          Enter your eamil address used during registration.
+        </span>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="py-4 flex flex-col gap-5"
+        >
+          <TextInput
+            name="eamil"
+            placeholder="email@example.com"
+            type="email"
+            register={register("email", {
+              required: "Email address is required.",
+            })}
+            styles="w-full rounded-lg"
+            labelStyle="ml-2"
+            error={errors?.email ? errors.email.message : ""}
+          />
+          {errorMessage?.message && (
+            <span
+              role="alert"
+              className={`text-sm ${
+                errorMessage?.status === "failed"
+                  ? "text-[#f64949fe]"
+                  : "text-[#2ba150fe]"
+              } mt-0.5`}
+            >
+              {errorMessage?.message}
+            </span>
+          )}
+          {isSubmitting ? (
+            <Loading />
+          ) : (
+            <CustomButton
+              type="submit"
+              containerStyles={`inline-flex justify-center rounded-md bg-blue px-8 py-3 text-sm font-medium text-white outline-none`}
+              title="Submit"
+            />
+          )}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ResetPassword;
