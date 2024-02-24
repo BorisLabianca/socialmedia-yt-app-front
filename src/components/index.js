766c46f2f30393c894a3c8f229b0a41624e3654b
Boris Labianca <boris.labianca@hotmail.fr>
2024-02-24 07:50:13 -0700

    created friends card component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Loading from "./Loading";
 import CustomButton from "./CustomButton";
 import ProfileCard from "./ProfileCard";
 import TopBar from "./TopBar";
+import FriendsCard from "./FriendsCard";
 
-export { TextInput, Loading, CustomButton, ProfileCard, TopBar };
+export { TextInput, Loading, CustomButton, ProfileCard, TopBar, FriendsCard };
